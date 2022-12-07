cc96689a886441a28d3ef484eb3af965ffd666b4
sol <osunpine@gmail.com>
2022-12-07 22:06:58 +0900

    feat. query mutation 성공 시, 라우팅 처리

--- src/hooks/useSignUpQuery.ts
+++ src/hooks/useSignUpQuery.ts
@@ -1,8 +1,13 @@
 import {useMutation} from "react-query";
 import {postSignUp} from "../routes/apis";
+import {useNavigate} from "react-router-dom";
 
 const useSignUpQuery = (id: string, pwd: string, code: string) => {
-    return useMutation(()=>postSignUp(id, pwd, code))
+    const navigate = useNavigate();
+
+    return useMutation(()=>postSignUp(id, pwd, code),{
+        onSuccess: () => navigate('/login')
+    })
 }
 
 export default useSignUpQuery;
