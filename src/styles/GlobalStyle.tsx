abffd08f460e3368f6c57fa37ce85a7cfb389d38
sol <osunpine@gmail.com>
2022-12-09 14:44:02 +0900

    feat. 전역 스타일 코드 추가

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -2,38 +2,51 @@ import { createGlobalStyle } from "styled-components";
 import theme from "./theme";
 
 const GlobalStyle = createGlobalStyle`
-* {
+  * {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
     margin: 0;
     padding: 0;
     border: 0;
     font-size: 100%;
     font-weight: 400;
-    color: ${theme.main}
+    color: ${theme.main};
+    box-sizing: border-box;
   }
+
   *::before, *::after {
     margin: 0;
     padding: 0;
   }
+
   article, aside, details, figcaption, figure,
   footer, header, hgroup, menu, nav, section {
     display: block;
   }
+
   ol, ul {
     list-style: none;
   }
+
   img {
     border: 0;
   }
+
   a {
     text-decoration: none;
   }
-  button{
-    border:none;
-    background:none;
+
+  button {
+    border: none;
+    background: none;
     cursor: pointer;
   }
-  input{
-    :focus{
+
+  input {
+    :focus {
       outline: .5px solid ${theme.main};
     }
   }
