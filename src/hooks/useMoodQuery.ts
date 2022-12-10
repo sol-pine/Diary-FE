9abb9f628e513aeff601b28b8beb8eeec516c132
sol <osunpine@gmail.com>
2022-12-10 16:43:02 +0900

    feat. 쿼리 옵션 추가

--- src/hooks/useMoodQuery.ts
+++ src/hooks/useMoodQuery.ts
@@ -2,7 +2,9 @@ import {useQuery} from "react-query";
 import {getMood} from "../routes/apis";
 
 const useMoodQuery = (year: number, month: number, date: number) => {
-    return useQuery(['mood', {year: year, month: month, date: date}], () => getMood(year, month, date))
+    return useQuery(['mood', {year: year, month: month, date: date}], () => getMood(year, month, date),{
+        refetchOnWindowFocus:false
+    })
 }
 
 export default useMoodQuery;
