97b5d3786bd2d7afdd1f9b07c183a48a85ec2b84
sol <osunpine@gmail.com>
2022-12-11 22:39:58 +0900

    feat. 무드 삭제 개발

--- src/hooks/query/useDeleteQuery.ts
+++ src/hooks/query/useDeleteQuery.ts
@@ -1,11 +1,15 @@
 import {useMutation, useQueryClient} from "react-query";
 import {deleteMood} from "../../routes/apis";
+import {setModalState} from "../../redux/modules/modalSlice";
+import {useAppDispatch} from "../../redux/store";
 
 const useDeleteQuery = (year: number, month: number, date: number) => {
-    const queryClient = useQueryClient()
+    const queryClient = useQueryClient();
+    const dispatch = useAppDispatch();
 
     return useMutation(() => deleteMood(year, month, date), {
-        onSuccess: () => queryClient.invalidateQueries('mood')
+        onSuccess: () => queryClient.invalidateQueries('moods')
+            .then(()=> dispatch(setModalState(false)))
     })
 }
 
