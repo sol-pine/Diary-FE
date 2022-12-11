077b40f379d2c98fc8c676b703736f506f12030e
sol <osunpine@gmail.com>
2022-12-11 22:40:35 +0900

    feat. 날짜 조회 custom hook 개발 및 테스트 코드 작성

--- src/hooks/useToday.test.ts
+++ src/hooks/useToday.test.ts
@@ -1,4 +1,4 @@
 import useToday from "./useToday";
 
 test('오늘 날짜를 구하는 custom hook',
-    () => expect(useToday()).toStrictEqual({'year': 2022, 'month': 12, 'date': 9, 'day': '금'}))
+    () => expect(useToday()).toStrictEqual({'year': 2022, 'month': 12, 'date': 11, 'day': 0}))
--- src/hooks/useToday.ts
+++ src/hooks/useToday.ts
@@ -1,11 +1,10 @@
 const useToday = () => {
     const days = new Date()
-    const weekLabel = ['일', '월', '화', '수', '목', '금', '토']
 
     const year = days.getFullYear()
     const month = days.getMonth()+1
     const date = days.getDate()
-    const day = weekLabel[days.getDay()]
+    const day = days.getDay()
 
     return {year: year, month: month, date:date, day:day}
 }
