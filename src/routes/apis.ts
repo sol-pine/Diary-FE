2e2375a72ee60cf217ea3b803bdda5116158c542
sol <osunpine@gmail.com>
2022-12-11 22:41:29 +0900

    feat. api 추가

--- src/routes/apis.ts
+++ src/routes/apis.ts
@@ -12,13 +12,17 @@ export const postLogIn = (id: string, pwd: string) =>
 export const putPwd = (id: string, code: string, newPwd: string) =>
     axiosInstance.put(`/reset`, {id: id, code: code, newPwd: newPwd})
 
+// 탈퇴
+export const deleteUser = () =>
+    axiosInstance.delete(`/user`)
+
 // 무드 기록
-export const postMood = (year: number, month: number, date: number, day: string, moodText: string, color: string) =>
-    axiosInstance.post(`/mood`, {year: year, month: month, date: date, day: day, moodText: moodText, color: color})
+export const postMood = (year: number, month: number, date: number, moodText: string, color: string) =>
+    axiosInstance.post(`/mood`, {year: year, month: month, date: date, moodText: moodText, color: color})
 
 // 하루 무드 조회
 export const getMood = (year: number, month: number, date: number) =>
-    axiosInstance.get(`/mood`,{params: {year, month, date}})
+    axiosInstance.get(`/mood`, {params: {year, month, date}})
 
 // 무드 수정
 export const putMood = (year: number, month: number, date: number, moodText: string, color: string) =>
@@ -26,4 +30,12 @@ export const putMood = (year: number, month: number, date: number, moodText: str
 
 // 무드 삭제
 export const deleteMood = (year: number, month: number, date: number) =>
-    axiosInstance.delete('/mood',{params: {year, month, date}})
+    axiosInstance.delete('/mood', {params: {year, month, date}})
+
+// 일년 무드 조회
+export const getYearlyMoods = (year: number) =>
+    axiosInstance.get(`/moods/${year}`)
+
+// 월별 무드 조회
+export const getMonthlyMoods = (year: number, month:number) =>
+    axiosInstance.get(`/moods`, {params: {year, month}})
